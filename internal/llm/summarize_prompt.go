54dd9cd7814b003fd6a5d1d790a1bc93ce2339c5
Masato Naka <masatonaka1989@gmail.com>
2025-01-16 10:00:17 +0900

    chore: update summarize prompt

--- internal/llm/summarize_prompt.go
+++ internal/llm/summarize_prompt.go
@@ -5,9 +5,10 @@ const (
 The summary will be used to retrieve relevant files to answer a user's question.
 Please write the summary in the following manner:
 
-- What is the code for? (e.g., "This function calculates the sum of two numbers.", "The document about package management in Go.", etc)
-- Type of content: (e.g., "Code", "Documentation", "Article", etc)
-- Function names: (e.g., "calculateSum", "main", etc)
+- What is the code for? (e.g. "This function calculates the sum of two numbers.", "The document about package management in Go.", etc)
+- Type of content: (e.g. "Code", "Documentation", "Article", etc)
+- Function names: (e.g. "calculateSum", "main", etc)
+- Imported packages, libraries, or modules: (e.g. "fmt", "math", etc)
 - References: where this code is used or referenced.
 - Any other relevant information
 
