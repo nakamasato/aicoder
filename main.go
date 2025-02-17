cf1a5d96c9c7be94bd932a6055c0e707d4483423
Masato Naka <masatonaka1989@gmail.com>
2025-02-18 07:01:36 +0900

    fix

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/nakamasato/aicoder/cmd"
+import (
+	"os"
+
+	"github.com/nakamasato/aicoder/cmd"
+)
 
 func main() {
-	cmd.Execute()
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
